82d232dcb4c8dcca7b95b54e2655d70e0d7edbdb
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-02-19 14:35:13 +0200

    fix: bugs on paths and static folder

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
-const config = require('config');
+const mongoose = require("mongoose");
+const config = require("config");
 
-const db = config.get('mongoURI');
+const db = config.get("mongoURI");
+mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        });
-    }
-    catch( err ) {
-        // Exit process with failure
-        process.exit(1);
-    }
-}
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+		});
+		console.log("DB CONNECTED");
+	} catch (err) {
+		// Exit process with failure
+		console.error(err.message);
+		process.exit(1);
+	}
+};
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const express = require('express')
-const config = require('config');
-const http = require('http');
-const socketio = require('socket.io');
-const connectDB = require('./db');
-const minesweeperController = require('./client/minesweeper/socket-controller');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const config = require("config");
+const http = require("http");
+const socketio = require("socket.io");
+const connectDB = require("./db");
+const minesweeperController = require("./socket-controller");
+const cookieParser = require("cookie-parser");
 
 // Create Express Socket.io server
 const app = express();
-const server = http.createServer( app );
-const io = socketio( server );
+const server = http.createServer(app);
+const io = socketio(server);
 
 connectDB();
 
 // Connect sockets to minesweeper logic
-io.on( 'connection', minesweeperController );
+io.on("connection", minesweeperController);
 
-app.use(express.json( { extended:false } ));
+app.use(express.json({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(__dirname));
 
 // Set Routes
-app.use('/minesweeper/register', require( './routes/register' ));
-app.use('/minesweeper/login', require( './routes/login' ));
-app.use('/minesweeper/game', require( './routes/game' ));
+app.use("/minesweeper/register", require("./routes/register"));
+app.use("/minesweeper/login", require("./routes/login"));
+app.use("/minesweeper/game", require("./routes/game"));
 
 const PORT = 3000; // config.get('PORT')
 
-const listener = server.listen( PORT, () => {
-    console.log( "Node.js listening on port " + listener.address().port );
+const listener = server.listen(PORT, () => {
+	console.log("Node.js listening on port " + listener.address().port);
 });

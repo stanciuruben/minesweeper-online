a7a7ab79362b4950f85f592fd779fbb89a19debd
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-03-04 11:53:16 +0200

    conf29

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, { path: '/minesweeper'});
 
 connectDB();
 
